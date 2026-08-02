1c0c52bbd51807993bcab99dba21289049ac64d6
jm bautista <jmbautistaa0428@gmail.com>
2026-08-02 21:02:48 +0800

    Ready for production

---
+++ public/build/assets/spinner-C0OoKLI4.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-C6KQPk4Q.js";import{c as i}from"./utils-D1lUiF-v.js";import{L as o}from"./loader-circle-Dk0M8Cm6.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
--- public/build/assets/spinner-DPJjbfuT.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-Dq9QDf4i.js";import{c as i}from"./utils-aS4ntags.js";import{L as o}from"./loader-circle-DQLQyoXe.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
