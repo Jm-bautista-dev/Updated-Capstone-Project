64bf45a9b706f971188adc8de2b85e378c1ca785
jm bautista <jmbautistaa0428@gmail.com>
2026-08-13 22:56:21 +0800

    reviews

--- public/build/assets/chevron-left-Cnkf4lWj.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./button-CxRl8-dF.js";const o=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],c=e("ChevronLeft",o);export{c as C};
--- public/build/assets/spinner-DNOjJUP0.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./app-BE6v46sX.js";import{c as i}from"./utils-y0LrMRml.js";import{L as o}from"./loader-circle-B9mnOj4c.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
