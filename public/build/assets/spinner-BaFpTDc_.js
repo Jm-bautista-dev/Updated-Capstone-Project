bf183a36dbd4886d7edd244759b17dc7fa0f052a
jm bautista <jmbautistaa0428@gmail.com>
2026-09-06 03:38:31 +0800

    build: compile production frontend assets

---
+++ public/build/assets/spinner-BaFpTDc_.js
@@ -0,0 +1,1 @@
+import{j as s}from"./app-ChE-ZS4p.js";import{c as i}from"./utils-jzauJGNx.js";import{L as o}from"./loader-circle-zB8rA6yM.js";function m({className:r,...a}){return s.jsx(o,{role:"status","aria-label":"Loading",className:i("size-4 animate-spin",r),...a})}export{m as S};
